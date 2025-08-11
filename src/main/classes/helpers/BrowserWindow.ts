63b7cf49bb452b17c28cccc70ebe2202f3d24b1d
Neo Haggard <80315475+Benzo-Fury@users.noreply.github.com>
2025-08-12 09:39:41 +1000

    fix(browser-window): proper icon management

--- src/main/classes/helpers/BrowserWindow.ts
+++ src/main/classes/helpers/BrowserWindow.ts
@@ -10,16 +10,16 @@ import Asset from "./Asset";
  * Extended BrowserWindow class for additional functionality.
  */
 export default class BrowserWindow extends ElectronBrowserWindow {
-  // Must be static to be accessed inside
+  // Must be static to be accessed inside constructor 
   private static defaultOptions: BrowserWindowConstructorOptions = {
     title: "Synta",
     width: 1200,
     height: 800,
     minWidth: 800,
     minHeight: 600,
-    icon: this.defaultIco(),
     show: false, // Don't show until ready
     center: true,
+    icon: BrowserWindow.defaultIco(),
     webPreferences: {
       sandbox: true,
       spellcheck: false,
@@ -38,8 +38,8 @@ export default class BrowserWindow extends ElectronBrowserWindow {
   /**
    * Gets the default app icon relavent to the OS.
    */
-  private static defaultIco(): NativeImage {
+  private static defaultIco() {
     // Todo:
-    return new Asset("icon.ico").toNativeImage();
+    return new Asset("logo.ico").toPath();
   }
 }
