4c401c113e7e1c87d06375a3aa6c83bc23357d8e
Neo Haggard <80315475+Benzo-Fury@users.noreply.github.com>
2025-08-12 20:51:23 +1000

    refractor: pretty please

--- src/main/classes/helpers/Asset.ts
+++ src/main/classes/helpers/Asset.ts
@@ -45,7 +45,7 @@ export default class Asset {
    */
   public static formPath(relPath: string) {
     const basePath = swallow(app.getAppPath, process.cwd());
-    
+
     return join(basePath, Asset.defaultDir, relPath);
   }
 }
