ba6458cfd1f90212065fa2b64538737c8233421b
Neo Haggard <80315475+Benzo-Fury@users.noreply.github.com>
2025-08-26 08:24:53 +1000

    feat: component management structure

--- src/main/classes/channels/Component.ts
+++ src/main/classes/channels/Component.ts
@@ -1,10 +1,30 @@
+import type { IpcMainInvokeEvent } from "electron";
 import Namespace, { Action } from "../../types/abstracts/Namespace";
 import { RegisterNamespace } from "../services/IPCHandler";
 
 @RegisterNamespace
 export default class Component extends Namespace {
   @Action
-  delete() {
+  sync(event: IpcMainInvokeEvent, id: string, code: string) { // Perhaps wrap id and code in a obj
+    // Sync component from renderer to disk
+
+    /*
+
+    components.sync(id, code);
+    
+    */
+  }
+
+  @Action
+  delete(event: IpcMainInvokeEvent) {
     return "Component deleted";
   }
 }
+
+// NEEDS:
+// We need to be able to:
+// - Sync components bidirectionally (renderer <-> disk)
+// - Renderer needs to be able to request a SINGULAR file code (to display & start editing)
+
+
+// We need to be able to select a project.
